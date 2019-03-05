f9bad6c4bb0b1aec43751269220a353473cdd13d
Iben A <cph-ia62@cphbusiness.dk>
2019-03-05 14:08:49 +0100

    Tilføjet DBConnect med kode

--- cupcake/src/main/java/data/DBConnector.java
+++ cupcake/src/main/java/data/DBConnector.java
@@ -1,9 +1,30 @@
 package data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author ibenk
  */
 public class DBConnector {
-    
+
+    private static String driver    = "com.mysql.cj.jdbc.Driver";
+    private static String url       = "jdbc:mysql://localhost:3306/cupcake";
+    private static String user      = "root";
+    private static String password  = "root";
+    private static Connection conn  = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Class.forName(driver);
+                conn = DriverManager.getConnection(url, user, password);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        return conn;
+    }
 }
