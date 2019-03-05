26ce024f57470df40f164a98e66dba595e2c3e49
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-03-05 15:54:42 +0100

    command created and class updated

---
+++ cupcake/src/main/java/command/UnknownCommand.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package command;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ndupo
+ */
+public class UnknownCommand extends Command
+{
+    public void execute(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1></h1>");
+            out.println("<p>Unknown Command!</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+}
