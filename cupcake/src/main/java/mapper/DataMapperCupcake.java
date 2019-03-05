38385ae5bb33c42ea8880b747fdd1203789ed48c
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-03-05 14:54:09 +0100

    datamapperCupcake updated

--- cupcake/src/main/java/mapper/DataMapperCupcake.java
+++ cupcake/src/main/java/mapper/DataMapperCupcake.java
@@ -5,14 +5,171 @@
  */
 package mapper;
 
+import entity.Bottom;
+import entity.Cupcake;
+import entity.Top;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author ibenk
  */
 public class DataMapperCupcake {
     
-    public static void main(String[] args) {
-        System.out.println("Hey");
+   private DB conn;
+    
+    public void addTopping(String name, int price) throws SQLException
+             {
+        conn = new DB();
+
+        String insertTopping = "INSERT INTO `cupcake`.`topping` (name, price) "
+                + "VALUES (?, " + price + ");";
+        PreparedStatement ps = conn.getConnection().prepareStatement(insertTopping);
+        ps.setString(1, name);
+        ps.executeUpdate();
+    }
+
+    public void addBottom(String name, int price) throws SQLException
+            {
+        conn = new DB();
+
+        String insertBottom = "INSERT INTO `cupcake`.`bottom` (name, price) "
+                + "VALUES (?, " + price + ");";
+        PreparedStatement ps = conn.getConnection().prepareStatement(insertBottom);
+        ps.setString(1, name);
+        ps.executeUpdate();
+    }
+    
+        public Cupcake makeCupcake(String topName, String bottomName) {
+
+        Top top = getTop(topName);
+        Bottom bottom = getBottom(bottomName);
+        return new Cupcake(top, bottom);
+
+    }
+
+    public List<Top> getTops() {
+        try {
+            conn = new DB();
+
+            String query = "SELECT * FROM `Cupcake`.`topping`;";
+
+            Connection connection = DB.getConnection();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            List<Top> toppings = new ArrayList<>();
+            String name = "";
+            int price = 0;
+
+            while (rs.next()) {
+                name = rs.getString("name");
+                price = rs.getInt("price");
+                Top top = new Top(name, price);
+                toppings.add(top);
+            }
+            return toppings;
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return null;
+    }
+
+    public List<Bottom> getBottoms() {
+        try {
+            conn = new DB();
+
+            String query = "SELECT * FROM `Cupcake`.`bottom`;";
+
+            Connection connection = conn.getConnection();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            List<Bottom> bottoms = new ArrayList<>();
+            String name = "";
+            int price = 0;
+
+            while (rs.next()) {
+                name = rs.getString("name");
+                price = rs.getInt("price");
+                Bottom bottom = new Bottom(name, price);
+                bottoms.add(bottom);
+            }
+            return bottoms;
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return null;
+    }
+
+    public int getTopPrice(String name) {
+        try {
+            conn = new DB();
+
+            String query = "SELECT price FROM `Cupcake`.`topping` "
+                    + "WHERE `cupcake`.`topping`.`name` = '" + name + "';";
+
+            Connection connection = conn.getConnection();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            int price = 0;
+
+            while (rs.next()) {
+                price = rs.getInt("price");
+            }
+            return price;
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return 0;
+    }
+
+    public int getBottomPrice(String name) {
+        try {
+            conn = new DB();
+
+            String query = "SELECT price FROM `Cupcake`.`bottom` "
+                    + "WHERE `cupcake`.`bottom`.`name` = '" + name + "';";
+
+            Connection connection = conn.getConnection();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            int price = 0;
+
+            while (rs.next()) {
+                price = rs.getInt("Price");
+            }
+            return price;
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return 0;
+    }
+
+    private Top getTop(String name) {
+         if (name != null) {
+            int price = getTopPrice(name);
+            Top top = new Top(name, price);
+            return top;
+        }
+        return null;
+    }
+
+    private Bottom getBottom(String name) {
+         if (name != null) {
+            int price = getBottomPrice(name);
+            Bottom bot = new Bottom(name, price);
+            return bot;
+        }
+        return null;
     }
     
 }
