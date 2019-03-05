0d0f63c13be12c57455e02e5c75068185f953956
Iben A <cph-ia62@cphbusiness.dk>
2019-03-05 14:21:25 +0100

    Tilføjet DataMapperUsers, tilføjet DataMapperCupcake

---
+++ cupcake/src/main/java/data/DataMapperUsers.java
@@ -0,0 +1,59 @@
+package data;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ibenk
+ */
+public class DataMapperUsers {
+    
+    public Users getUser(String username) throws SQLException {
+        try {
+            DBConnector conn = new DBConnector();
+            Statement statement = conn.getConnection().createStatement();
+            String query
+                    = "SELECT * "
+                    + "FROM cupcake.user "
+                    + "WHERE username='" + username + "'";
+            
+            ResultSet rs = statement.executeQuery(query);
+            Users user = null;
+            int id;
+            String password;
+            int balance;
+            String email;
+            
+            while(rs.next()) {
+                id = rs.getInt("iduser");
+                password = rs.getString("password");
+                balance = rs.getInt("balance");
+                email = rs.getString("email");
+                
+                user = new Users(id, username, password, balance, email);
+            }
+            return user;
+            
+        } catch (Exception ex) {
+            Logger.getLogger(DataMapperUsers.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public void createUser(String username, String password, String email) throws SQLException {
+        DBConnector conn = new DBConnector();
+
+        String createUser 
+                = "INSERT INTO cupcake.user"
+                + "VALUES (" + username + ", " + password + ", 0, " + email + ");";
+        PreparedStatement ps = conn.getConnection().prepareStatement(createUser);
+        ps.executeUpdate();
+    }
+    
+
+}
