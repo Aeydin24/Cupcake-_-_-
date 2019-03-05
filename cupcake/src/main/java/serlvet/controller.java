c3f2934d7c248f6fca4bf71d0aee994938b1c610
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-03-05 14:22:28 +0100

    controller updated

--- cupcake/src/main/java/serlvet/controller.java
+++ cupcake/src/main/java/serlvet/controller.java
@@ -13,7 +13,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//Hej
+
 /**
  *
  * @author ndupo
